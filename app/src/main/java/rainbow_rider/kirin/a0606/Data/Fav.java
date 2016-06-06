711dd3f664e49d67284e6797509c9cc692e4095f
acque2two <acque.2two@gmail.com>
2016-06-06 18:55:37 +0900

    [Fix] UserAnsの追加・Favの追加

---
+++ app/src/main/java/rainbow_rider/kirin/a0606/Data/Fav.java
@@ -0,0 +1,14 @@
+package rainbow_rider.kirin.a0606.Data;
+
+/**
+ * Created by acq on 16/06/02.
+ * Title:
+ * Author:
+ * Memo:
+ * Todo:
+ */
+
+public class Fav{
+    private Genre genre;
+    private User user;
+}
---
+++ app/src/main/java/rainbow_rider/kirin/a0606/Data/UserAns.java
@@ -0,0 +1,15 @@
+package rainbow_rider.kirin.a0606.Data;
+
+/**
+ * Created by acq on 16/06/02.
+ * Title:
+ * Author:
+ * Memo:
+ * Todo:
+ */
+
+public class UserAns {
+    private User user;
+    private Genre genre;
+    private long ans_id;
+}
