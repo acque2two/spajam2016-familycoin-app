d55203428adcf2bb64c7d8be4c339e1448683151
acque2two <acque.2two@gmail.com>
2016-06-07 18:53:06 +0900

    [Add] QuestionUserRankList

---
+++ app/src/main/java/rainbow_rider/kirin/a0606/transfer/question/QuestionUserRankList.java
@@ -0,0 +1,16 @@
+package rainbow_rider.kirin.a0606.transfer.question;
+
+import rainbow_rider.kirin.a0606.transfer.Sender;
+
+/**
+ * Created by acq on 16/06/07.
+ * Title:
+ * Author:
+ * Memo:
+ * Todo:
+ */
+public class QuestionUserRankList extends Sender {
+    public QuestionUserRankList(){
+        super.setPath("/questionuserrank" );
+    }
+}
