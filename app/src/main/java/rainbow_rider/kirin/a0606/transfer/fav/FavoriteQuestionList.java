e4275a6936b5626b704765330b92c6875940e8cc
acque2two <acque.2two@gmail.com>
2016-06-07 18:54:13 +0900

    [Add] FavoriteAdd

---
+++ app/src/main/java/rainbow_rider/kirin/a0606/transfer/fav/FavoriteQuestionList.java
@@ -0,0 +1,17 @@
+package rainbow_rider.kirin.a0606.transfer.fav;
+
+import rainbow_rider.kirin.a0606.transfer.Sender;
+
+/**
+ * Created by acq on 16/06/07.
+ * Title:
+ * Author:
+ * Memo:
+ * Todo:
+ */
+
+public class FavoriteQuestionList extends Sender{
+    public FavoriteQuestionList(){
+        super.setPath( "/favoriteuserlist" );
+    }
+}
---
+++ app/src/main/java/rainbow_rider/kirin/a0606/transfer/fav/FavoriteUserList.java
@@ -0,0 +1,17 @@
+package rainbow_rider.kirin.a0606.transfer.fav;
+
+import rainbow_rider.kirin.a0606.transfer.Sender;
+
+/**
+ * Created by acq on 16/06/07.
+ * Title:
+ * Author:
+ * Memo:
+ * Todo:
+ */
+
+public class FavoriteUserList extends Sender{
+    public FavoriteUserList(){
+        super.setPath( "/favoriteuserlist" );
+    }
+}
