5cbe4f033c2370a28f6ba1348a65e39c285b975e
acque2two <acque.2two@gmail.com>
2016-06-07 12:21:41 +0900

    [Add] ありそうな問題系の追加

---
+++ app/src/main/java/rainbow_rider/kirin/a0606/transfer/question/QuestionAdd.java
@@ -0,0 +1,17 @@
+package rainbow_rider.kirin.a0606.transfer.question;
+
+import rainbow_rider.kirin.a0606.transfer.Sender;
+
+/**
+ * Created by acq on 16/06/07.
+ * Title:
+ * Author:
+ * Memo:
+ * Todo:
+ */
+
+public class QuestionAdd extends Sender{
+    public QuestionAdd(){
+        super.setPath( "/questionadd" );
+    }
+}
