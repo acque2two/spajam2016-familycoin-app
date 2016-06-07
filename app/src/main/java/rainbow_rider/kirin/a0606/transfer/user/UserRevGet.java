3fe34d027f54fb82c83fa608516a45fa80a18ba2
acque2two <acque.2two@gmail.com>
2016-06-07 18:50:41 +0900

    [Add] UserRevGet

---
+++ app/src/main/java/rainbow_rider/kirin/a0606/transfer/user/UserRevGet.java
@@ -0,0 +1,17 @@
+package rainbow_rider.kirin.a0606.transfer.user;
+
+import rainbow_rider.kirin.a0606.transfer.Sender;
+
+/**
+ * Created by acq on 16/06/07.
+ * Title:
+ * Author:
+ * Memo:
+ * Todo:
+ */
+
+public class UserRevGet extends Sender{
+    public UserRevGet(){
+        super.setPath( "/userrevget" );
+    }
+}
