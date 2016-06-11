b2ad5bd1c7ac696d991c45470bffe80e92fc345a
acque2two <acque.2two@gmail.com>
2016-06-11 18:14:40 +0900

    [Add] worklistの修正

--- app/src/main/java/rainbow_rider/kirin/spajam/transfer/sync/work/WorkList.java
+++ app/src/main/java/rainbow_rider/kirin/spajam/transfer/sync/work/WorkList.java
@@ -1,5 +1,9 @@
 package rainbow_rider.kirin.spajam.transfer.sync.work;
 
+import java.util.ArrayList;
+
+import rainbow_rider.kirin.spajam.Data.Data;
+import rainbow_rider.kirin.spajam.Data.Family;
 import rainbow_rider.kirin.spajam.transfer.sync.Sender;
 
 /**
@@ -11,7 +15,11 @@ import rainbow_rider.kirin.spajam.transfer.sync.Sender;
  */
 public class WorkList extends Sender {
 
-    public WorkList( ) {
+    public WorkList( Family family ) {
+        ArrayList<Family> familyList = new ArrayList<>();
+        familyList.add( family );
+        allData = new Data();
+        allData.setFamily( familyList );
         super.setPath( "/worklist" );
     }
 
