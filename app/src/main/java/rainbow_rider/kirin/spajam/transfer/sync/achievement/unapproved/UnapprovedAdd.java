26b0e51ef80fa72df6398408b68684784e47ae71
acque2two <acque.2two@gmail.com>
2016-06-11 18:28:50 +0900

    [Add] 同期版の実績を追加

---
+++ app/src/main/java/rainbow_rider/kirin/spajam/transfer/sync/achievement/unapproved/UnapprovedAdd.java
@@ -0,0 +1,13 @@
+package rainbow_rider.kirin.spajam.transfer.sync.achievement.unapproved;
+
+import rainbow_rider.kirin.spajam.transfer.sync.Sender;
+
+/**
+ * Created by acq on 16/06/11.
+ * Title:
+ * Author:
+ * Memo:
+ * Todo:
+ */
+public class UnapprovedAdd extends Sender {
+}
