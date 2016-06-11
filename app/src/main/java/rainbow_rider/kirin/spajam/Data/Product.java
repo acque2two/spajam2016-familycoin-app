38bb8e4518b5b5f8163ba3a0d51e468bdffbf879
acque2two <acque.2two@gmail.com>
2016-06-12 08:35:15 +0900

    adsfoh

--- app/src/main/java/rainbow_rider/kirin/spajam/Data/Product.java
+++ app/src/main/java/rainbow_rider/kirin/spajam/Data/Product.java
@@ -10,5 +10,40 @@ import java.io.Serializable;
  * Todo:
  */
 public class Product implements Serializable {
+    public Integer p_id;
+    public String p_name;
+    public String f_id;
+    public Integer p_point;
 
+    public Integer getP_point( ) {
+        return p_point;
+    }
+
+    public void setP_point( Integer p_point ) {
+        this.p_point = p_point;
+    }
+
+    public String getF_id( ) {
+        return f_id;
+    }
+
+    public void setF_id( String f_id ) {
+        this.f_id = f_id;
+    }
+
+    public String getP_name( ) {
+        return p_name;
+    }
+
+    public void setP_name( String p_name ) {
+        this.p_name = p_name;
+    }
+
+    public Integer getP_id( ) {
+        return p_id;
+    }
+
+    public void setP_id( Integer p_id ) {
+        this.p_id = p_id;
+    }
 }
