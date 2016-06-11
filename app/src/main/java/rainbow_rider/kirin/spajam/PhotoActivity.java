2be586609066ca9b70b3f893d28ac25d082f62f8
nozomi <nontan4061@gmail.com>
2016-06-12 03:43:59 +0900

    [Update] MainActivity

--- app/src/main/java/rainbow_rider/kirin/spajam/PhotoActivity.java
+++ app/src/main/java/rainbow_rider/kirin/spajam/PhotoActivity.java
@@ -1,16 +1,10 @@
 package rainbow_rider.kirin.spajam;
 
 import android.content.Intent;
-import android.provider.ContactsContract;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Switch;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class PhotoActivity extends AppCompatActivity {
 
