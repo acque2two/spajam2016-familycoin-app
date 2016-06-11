2d346fd7795db26da7772e45d593b8b43e95bb35
acque2two <acque.2two@gmail.com>
2016-06-11 18:29:44 +0900

    [Add] 非同期版の実績を追加

---
+++ app/src/main/java/rainbow_rider/kirin/spajam/transfer/async/achievement/achievement/AchievementAdd.java
@@ -0,0 +1,13 @@
+package rainbow_rider.kirin.spajam.transfer.async.achievement.achievement;
+
+import rainbow_rider.kirin.spajam.transfer.sync.Sender;
+
+/**
+ * Created by acq on 16/06/11.
+ * Title:
+ * Author:
+ * Memo:
+ * Todo:
+ */
+public class AchievementAdd extends Sender {
+}
