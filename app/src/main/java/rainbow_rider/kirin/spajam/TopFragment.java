a0b5fe16deeb8b3a900a61c8bdd84be330b76cc2
nozomi <nontan4061@gmail.com>
2016-06-12 11:18:01 +0900

    [update]ごめんなさい2

--- app/src/main/java/rainbow_rider/kirin/spajam/TopFragment.java
+++ app/src/main/java/rainbow_rider/kirin/spajam/TopFragment.java
@@ -146,9 +146,9 @@ public class TopFragment extends Fragment {
                 } else {
                     //mAdapter.addAll(reply.getFamily().get(0).getWork());
                     for ( int i = 0 ; i < reply.getFamily().size() ; i ++ ){
-                        hashTmp.put("getWork", reply.getFamily().get(0).getWork().get(i).toString() );
-                        hashTmp.put("u_data", reply.getFamily().get(0).getWork().get(i).toString() );
-                        hashTmp.put("sub", reply.getFamily().get(0).getWork().get(i).toString() );
+                        hashTmp.put("getWork", reply.getFamily().get(0).getWork().get(i).getW_name() );
+                        hashTmp.put("u_data", reply.getFamily().get(0).getWork().get(i).getW_text() );
+                        hashTmp.put("sub", reply.getFamily().get(0).getWork().get(i).getPoint().toString() + "Point" );
                         list_data.add(new HashMap<String, String>(hashTmp));
                         hashTmp.clear();
 
